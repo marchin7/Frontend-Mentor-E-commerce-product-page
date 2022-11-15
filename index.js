abca8870a1a622e3e38afc71cc08e1170bc6da72
marchin7 <114770259+marchin7@users.noreply.github.com>
2022-11-15 21:59:23 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 const cartHeader =document.getElementById('cart-header');
 const cart =document.getElementById('cart');
 
+//cartHeader.addEventListener('click', ()=>{
+ // if(cart.style.display === 'block') {
+ //   cart.style.display = 'none'
+  //}
+  //else {cart.style.display = 'block'}
+//})
+
+
+
 cartHeader.addEventListener('click', ()=>{
-  if(cart.style.display === 'block') {
-    cart.style.display = 'none'
+  if(cart.style.bottom === '-283px') {
+    cart.style.bottom = '100px'
   }
-  else {cart.style.display = 'block'}
+  else {cart.style.bottom = '-283px'}
 })
